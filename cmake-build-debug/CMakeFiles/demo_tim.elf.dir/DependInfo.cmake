23b2a1fbe3d6cd3127d0127646671c3d0c8f3bfd
Lightrain <lightrainmail@gmail.com>
2023-08-09 06:57:35 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/demo_tim.elf.dir/DependInfo.cmake
@@ -0,0 +1,66 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Core/Startup/startup_stm32l476rgtx.s" "C:/Users/14806/OneDrive/Desktop/demo_tim/cmake-build-debug/CMakeFiles/demo_tim.elf.dir/Core/Startup/startup_stm32l476rgtx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32L476xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Core/Inc"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Drivers/STM32L4xx_HAL_Driver/Inc"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Drivers/CMSIS/Device/ST/STM32L4xx/Include"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Core/Src/gpio.c" "CMakeFiles/demo_tim.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Core/Src/gpio.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Core/Src/main.c" "CMakeFiles/demo_tim.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Core/Src/main.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Core/Src/stm32l4xx_hal_msp.c" "CMakeFiles/demo_tim.elf.dir/Core/Src/stm32l4xx_hal_msp.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Core/Src/stm32l4xx_hal_msp.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Core/Src/stm32l4xx_it.c" "CMakeFiles/demo_tim.elf.dir/Core/Src/stm32l4xx_it.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Core/Src/stm32l4xx_it.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Core/Src/syscalls.c" "CMakeFiles/demo_tim.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Core/Src/syscalls.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Core/Src/sysmem.c" "CMakeFiles/demo_tim.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Core/Src/sysmem.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Core/Src/system_stm32l4xx.c" "CMakeFiles/demo_tim.elf.dir/Core/Src/system_stm32l4xx.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Core/Src/system_stm32l4xx.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Core/Src/tim.c" "CMakeFiles/demo_tim.elf.dir/Core/Src/tim.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Core/Src/tim.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Core/Src/usart.c" "CMakeFiles/demo_tim.elf.dir/Core/Src/usart.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Core/Src/usart.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_tim/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c.obj" "gcc" "CMakeFiles/demo_tim.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
